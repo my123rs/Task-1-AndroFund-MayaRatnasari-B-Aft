269198a56d9ae50378e7b2b509dfd5bd7478fda1
mayaratnasari <20210810098@uniku.ac.id>
2023-10-08 19:51:03 +0700

    MayaRatnasari

---
+++ LatihanMaya/app/src/main/java/com/example/latihanmaya/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.latihanmaya
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.latihanmaya.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var tv: TextView
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFragment1.setOnClickListener {replaceFragment(Fragment_1())}
+
+        binding.btnFragment2.setOnClickListener {replaceFragment(Fragment_2())}
+
+        tv = findViewById(R.id.text2)
+
+        val username = intent.getParcelableExtra<User>("User")?.username
+        val password = intent.getParcelableExtra<User>("User")?.password
+        tv.text = "Username : $username dan Password: $password"
+
+        val btnImplicit: Button = findViewById(R.id.btn_implicit)
+        btnImplicit.setOnClickListener(this)
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(androidx.fragment.R.id.fragment_container_view_tag, fragment)
+        fragmentTransaction.commit()
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_implicit -> {
+         //       val message = "Hallo!!"
+        //        val intent = Intent()
+        //        intent.action = Intent.ACTION_SEND
+        //        intent.putExtra(Intent.EXTRA_TEXT, message)
+        //        intent.type = "text/plain"
+        //        startActivity(intent)
+
+                val intent = Intent()
+                intent.putExtra("history", "Anda sudah Login")
+                setResult(RESULT_OK, intent)
+                finish()
+            }
+        }
+    }
+}
